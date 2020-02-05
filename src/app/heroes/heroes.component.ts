ae13541f36e6913e1c6fe4c78f1a81fd9697c638
SOE-Load <SOE-Load@VNPF0Z4EMX.CSCMWS.CSCMWS.COM>
2020-02-05 15:01:50 +0700

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,20 +13,24 @@ export class HeroesComponent implements OnInit {
   // hero = "Windstorm";
   hero: Hero = {
     id: 1,
-    name: "Windstorm"
+    name: 'Windstorm'
+  };
+  // heroes = HEROES;
+  heroes: Hero[]; // Service
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
-
-  selectedHero: Hero;
-  onSelect(hero: Hero) : void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes(); // getHeroes() 'HeroService class'; heroService is a parameter of contrustor
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
